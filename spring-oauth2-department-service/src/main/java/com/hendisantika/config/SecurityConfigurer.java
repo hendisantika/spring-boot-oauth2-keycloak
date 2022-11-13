7579bd9f08f793d26f3a3dcf803e1a0b74873266
Hendi Santika <hendisantika@yahoo.co.id>
2022-11-14 06:24:31 +0700

    #13 - Add corsConfigurationSource in SecurityConfigurer :pencil2: :dancer: :whale2:

--- spring-oauth2-department-service/src/main/java/com/hendisantika/config/SecurityConfigurer.java
+++ spring-oauth2-department-service/src/main/java/com/hendisantika/config/SecurityConfigurer.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -14,6 +15,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 /**
  * Created by IntelliJ IDEA.
@@ -59,5 +62,14 @@ public class SecurityConfigurer extends ResourceServerConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers(securityProperties.getApiMatcher())
                 .authenticated();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        if (null != securityProperties.getCorsConfiguration()) {
+            source.registerCorsConfiguration("/**", securityProperties.getCorsConfiguration());
+        }
+        return source;
     }
 }
