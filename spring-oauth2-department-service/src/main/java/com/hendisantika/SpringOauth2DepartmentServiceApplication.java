81324672dbc3c84c5c63ca78eb91f40422b86944
Hendi Santika <hendisantika@yahoo.co.id>
2022-11-14 06:15:11 +0700

    #2 - Init spring-oauth2-department-service project :pencil2: :dancer: :whale2:

---
+++ spring-oauth2-department-service/src/main/java/com/hendisantika/SpringOauth2DepartmentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOauth2DepartmentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOauth2DepartmentServiceApplication.class, args);
+    }
+
+}
