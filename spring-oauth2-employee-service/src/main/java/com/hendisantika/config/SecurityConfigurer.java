ae699ffc650a782e570163cf16cfd78eb729fbf5
Hendi Santika <hendisantika@yahoo.co.id>
2022-11-16 06:08:55 +0700

    #32 - Add SecurityConfigurer :pencil2: :green_book: :whale:

---
+++ spring-oauth2-employee-service/src/main/java/com/hendisantika/config/SecurityConfigurer.java
@@ -0,0 +1,39 @@
+package com.hendisantika.config;
+
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-oauth2-employee-service
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/16/22
+ * Time: 06:08
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+@EnableWebSecurity
+@EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@ConditionalOnProperty(prefix = "rest.security", value = "enabled", havingValue = "true")
+@Import({SecurityProperties.class})
+@RequiredArgsConstructor
+public class SecurityConfigurer extends ResourceServerConfigurerAdapter {
+
+    private static final Logger LOG = LoggerFactory.getLogger(SecurityConfigurer.class);
+
+    private final ResourceServerProperties resourceServerProperties;
+
+    private final SecurityProperties securityProperties;
+}
