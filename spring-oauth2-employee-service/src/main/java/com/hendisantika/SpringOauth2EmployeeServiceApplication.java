a66b4705671a6985ad90287eb4524a0ec6f6e2ed
Hendi Santika <hendisantika@yahoo.co.id>
2022-11-16 06:13:38 +0700

    #43 - Add EmployeeService :pencil2: :green_book: :whale:

--- spring-oauth2-employee-service/src/main/java/com/hendisantika/SpringOauth2EmployeeServiceApplication.java
+++ spring-oauth2-employee-service/src/main/java/com/hendisantika/SpringOauth2EmployeeServiceApplication.java
@@ -1,13 +1,29 @@
 package com.hendisantika;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Service;
 
 @SpringBootApplication
 public class SpringOauth2EmployeeServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringOauth2EmployeeServiceApplication.class, args);
+    }
+
+    @Service
+    @RequiredArgsConstructor
+    public static class EmployeeService {
+
+        private final DepartmentRestClient departmentRestClient;
+
+        public String getEmployeeAndDepartment() {
+            String employeeName = "Arun";
+            String departmentName = departmentRestClient.getDepartmentName();
+
+            return "Employee Service Returned: " + employeeName + ", \nDepartment Service Returned: " + departmentName;
+        }
     }
 
 }
