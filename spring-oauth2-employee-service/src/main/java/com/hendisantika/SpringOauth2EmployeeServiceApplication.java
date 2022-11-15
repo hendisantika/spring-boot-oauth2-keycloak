79d3c0e2ea3ea2e24dd980fd2783921c48ba1111
Hendi Santika <hendisantika@yahoo.co.id>
2022-11-16 06:14:09 +0700

    #44 - Add DepartmentRestClient :pencil2: :green_book: :whale:

--- spring-oauth2-employee-service/src/main/java/com/hendisantika/SpringOauth2EmployeeServiceApplication.java
+++ spring-oauth2-employee-service/src/main/java/com/hendisantika/SpringOauth2EmployeeServiceApplication.java
@@ -3,6 +3,8 @@ package com.hendisantika;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 @SpringBootApplication
@@ -23,6 +25,17 @@ public class SpringOauth2EmployeeServiceApplication {
             String departmentName = departmentRestClient.getDepartmentName();
 
             return "Employee Service Returned: " + employeeName + ", \nDepartment Service Returned: " + departmentName;
+        }
+    }
+
+    @Component
+    @RequiredArgsConstructor
+    public static class DepartmentRestClient {
+
+        private final OAuth2RestTemplate oAuth2RestTemplate;
+
+        public String getDepartmentName() {
+            return oAuth2RestTemplate.getForObject("http://localhost:8095/api/departments/1", String.class);
         }
     }
 
